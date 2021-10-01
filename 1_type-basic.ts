0b91a7ede6dbc7d72e720be9395a81788e12d8d7
어영부영살아가는개발자 <69393030+zuzu578@users.noreply.github.com>
2021-10-01 13:10:09 +0900

    type script Type!

---
+++ 1_type-basic.ts
@@ -0,0 +1,12 @@
+//js 
+var str ='hello';
+
+// ts 문자열 선언 방법 
+let str2: string = 'hello';
+let num: number = 10;
+// array 선언 -> 제네릭 
+let arr: Array<number> = [1,2,3];
+let arr2: Array<String> =['ctt','efef','fewfw'];
+
+
+
